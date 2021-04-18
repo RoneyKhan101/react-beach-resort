a18499f097224bbc15f8dca6ee8027fad115c30a
Roney Khan <ork.roney@gmail.com>
2021-04-18 09:00:55 +0600

    react beach application

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Hero>
         <Banner
           title="luxurious rooms"
-          subtitle="deluxe rooms starting at $299"
+          subtitle="deluxe rooms starting at $150"
         >
           <Link to="/rooms" className="btn-primary">
             our rooms
