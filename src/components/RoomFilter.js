acf4c1f637b4079ad5168fbddbe21aebf180a178
Roney Khan <ork.roney@gmail.com>
2021-04-18 08:35:39 +0600

    react beach application

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,157 @@
+import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
+
+//get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map((items) => items[value]))];
+};
+
+export default function RoomFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  //get unique types
+  let types = getUnique(rooms, "type");
+  //add all
+  types = ["all", ...types];
+  //map to jsx
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  //get unique guest
+  let people = getUnique(rooms, "capacity");
+  //add 1
+  
+  //map to jsx
+  people = people.map((item, index) => {
+    return (
+      <option vlaue={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            vlaue={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/* end select type */}
+
+        {/* guests */}
+        <div className="form-group">
+          <label htmlFor="capacity">Guests</label>
+          <select
+            name="capacity"
+            id="capacity"
+            vlaue={capacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+        {/* end guests */}
+
+        {/* room price */}
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        {/* end of room price*/}
+        {/* size*/}
+        <div className="form-group">
+          <label htmlFor="size">
+            room size <small>(sq.ft)</small>
+          </label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            ></input>
+
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            ></input>
+          </div>
+        </div>
+        {/* end of size*/}
+
+        {/* extras*/}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          {/* end of extras*/}
+          {/* extras*/}
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            ></input>
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+        {/* end of extras*/}
+      </form>
+    </section>
+  );
+}
---
+++ src/components/RoomList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Rooms from '../pages/Rooms'
+import Room from "./Room"
+
+export default function RoomList({room}) {
+
+    if(room.length === 0){
+        return(
+            <div className="empty-search">
+                <h1>Unfortunately no rooms matched your search parameters</h1>
+            </div>
+        )
+    }
+  return (
+    <section className="roomslist">
+        <div className="roomslist-center">
+            {
+                room.map(item => {
+                    return <Room key={item.id} room={item}/>
+                })
+            }
+        </div>
+
+    </section>
+  )
+}
